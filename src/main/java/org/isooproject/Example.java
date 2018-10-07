b51d24bc8385b1d860ad1746f29aaa9697f30cd1
leesoo <isoo7510@gmail.com>
2018-10-08 00:57:14 +0900

    di componentscan 대신 import(classname)으로 bean을 찾는것

--- src/main/java/org/isooproject/Example.java
+++ src/main/java/org/isooproject/Example.java
@@ -1,16 +1,18 @@
 package org.isooproject;
 
+import org.isooproject.configuration.ServiceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
 @Configuration
-@ComponentScan
+@Import(ServiceConfig.class)
 public class Example {
 
     @RequestMapping
