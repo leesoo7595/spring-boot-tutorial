57f35589c2b2d23ffa91fba10f11c735dbcbcbb0
leesoo <isoo7510@gmail.com>
2018-10-09 01:58:44 +0900

    mvn clean package & java -jar --debug

--- src/main/java/org/isooproject/Example.java
+++ src/main/java/org/isooproject/Example.java
@@ -2,6 +2,7 @@ package org.isooproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -10,10 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableAutoConfiguration
-@Configuration
-@ComponentScan
-@ImportResource("application.xml")
+@SpringBootApplication
 public class Example {
 
     @RequestMapping
