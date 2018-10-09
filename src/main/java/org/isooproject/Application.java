4c1ff823f694dc811e7e947e9e7fd0345bcbbb5e
leesoo <isoo7510@gmail.com>
2018-10-09 13:33:19 +0900

    .idea 제거

--- src/main/java/org/isooproject/Application.java
+++ src/main/java/org/isooproject/Application.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class Application {
 
-    @RequestMapping
+    @RequestMapping("/")
     String home() {
         return "Hello World!";
     }
