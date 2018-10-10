cf263e744985662b03c662c9335efecfd6122bcb
leesoo <isoo7510@gmail.com>
2018-10-11 00:14:34 +0900

    일부러 오류를 내서 로그 찍어보기

--- src/main/java/org/isooproject/hello/HelloService.java
+++ src/main/java/org/isooproject/hello/HelloService.java
@@ -2,10 +2,17 @@ package org.isooproject.hello;
 
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
 @Service
 public class HelloService {
 
     public String getMessage() {
         return "Hello, Spring boot!";
+    }
+
+    @PostConstruct
+    public void init() {
+        throw new RuntimeException("Intended Exception!");
     }
 }
