f1c31db0f71f8ea84e37250b0b0e25b790c0d39e
leesoo <isoo7510@gmail.com>
2018-10-11 12:56:58 +0900

    application listener

--- src/main/java/org/isooproject/Application.java
+++ src/main/java/org/isooproject/Application.java
@@ -23,6 +23,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(Application.class);
+        application.addListeners(new MyListener());
         application.run(args);
 //        SpringApplication.run(Application.class, args);
     }
