2ad31f3c6bc509299af288d3700f3240192a0c58
leesoo <isoo7510@gmail.com>
2018-10-11 13:06:36 +0900

    setWebApplicationType

--- src/main/java/org/isooproject/Application.java
+++ src/main/java/org/isooproject/Application.java
@@ -4,6 +4,7 @@ import org.isooproject.hello.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,9 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(Application.class);
         application.addListeners(new MyListener());
+        // spring 5 부터는 서블릿 & 리액티브 두가지로 나눠지면서 해당을 선택할 수 있게 된다.
+//        application.setWebApplicationType(WebApplicationType.NONE);
+//        application.setApplicationContextClass();
         application.run(args);
 //        SpringApplication.run(Application.class, args);
     }
