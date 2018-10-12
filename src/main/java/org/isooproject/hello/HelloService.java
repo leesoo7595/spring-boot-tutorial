6cee6683e83b7b63196c308cb05f3b035f8fcb1f
leesoo <isoo7510@gmail.com>
2018-10-13 00:40:31 +0900

    ApplicationRunner 2개 돌리기

--- src/main/java/org/isooproject/hello/HelloService.java
+++ src/main/java/org/isooproject/hello/HelloService.java
@@ -13,22 +13,7 @@ import java.util.stream.Collectors;
 @Service
 public class HelloService {
 
-    @Autowired
-    ApplicationArguments arguments;
-
-    @Value("${hello}")
-    String[] helloValues;
-
-    /**
-     * CLI : java -jar target/spring-boot-1.0-SNAPSHOT.jar --hello=hello --hello=world
-     * --hello=Hello -hello=World => {"Hello", "World"} => "Hello, World"
-     * @return
-     */
     public String getMessage() {
-//        List<String> helloValues = arguments.getOptionValues("hello");
-//        String collect = helloValues.stream().collect(Collectors.joining(","));
-//        return collect;
-        return Arrays.stream(helloValues).collect(Collectors.joining(", "));
-
+        return "hello world!";
     }
 }
