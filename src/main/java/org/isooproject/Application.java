4ddb15c06eb327d4fca8bafd150a67bc8547b8da
leesoo <isoo7510@gmail.com>
2018-10-13 01:19:16 +0900

    Application gracefully exit code 42

--- src/main/java/org/isooproject/Application.java
+++ src/main/java/org/isooproject/Application.java
@@ -3,9 +3,12 @@ package org.isooproject;
 import org.isooproject.hello.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +20,11 @@ public class Application {
     @Autowired
     HelloService helloService;
 
+    @Bean
+    public ExitCodeGenerator exitCodeGenerator() {
+        return () -> 42;
+    }
+
     @RequestMapping("/")
     String home() {
         return helloService.getMessage();
@@ -25,10 +33,9 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(Application.class);
         application.addListeners(new MyListener());
-        // spring 5 부터는 서블릿 & 리액티브 두가지로 나눠지면서 해당을 선택할 수 있게 된다.
-//        application.setWebApplicationType(WebApplicationType.NONE);
-//        application.setApplicationContextClass();
-        application.run(args);
-//        SpringApplication.run(Application.class, args);
+        ConfigurableApplicationContext run = application.run(args);
+
+        System.exit(SpringApplication.exit(run));
+
     }
 }
