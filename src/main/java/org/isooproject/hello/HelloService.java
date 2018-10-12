fb2d27f8f0d5f3ed4176bfe169a51f2b9b58e25a
leesoo <isoo7510@gmail.com>
2018-10-12 18:29:53 +0900

    Accessing Application Arguments

--- src/main/java/org/isooproject/hello/HelloService.java
+++ src/main/java/org/isooproject/hello/HelloService.java
@@ -1,18 +1,29 @@
 package org.isooproject.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class HelloService {
 
-    public String getMessage() {
-        return "Hello, Spring boot!";
-    }
+    @Autowired
+    ApplicationArguments arguments;
 
-//    @PostConstruct
-//    public void init() {
-//        throw new RuntimeException("Intended Exception!");
-//    }
+    /**
+     * --hello=Hello -hello=World => {"Hello", "World"} => "Hello, World"
+     * @return
+     */
+    public String getMessage() {
+        List<String> helloValues = arguments.getOptionValues("hello");
+        String collect = helloValues.stream().collect(Collectors.joining(","));
+        return collect;
+
+    }
 }
