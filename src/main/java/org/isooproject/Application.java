b403db0acfefc9a7b8db208a27ae22202aa47406
leesoo <isoo7510@gmail.com>
2018-10-14 01:12:38 +0900

    24. Externalized Configuration

--- src/main/java/org/isooproject/Application.java
+++ src/main/java/org/isooproject/Application.java
@@ -12,7 +12,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+/**
+ * --SPRING_APPLICATION_JSON={"foo":"bar"}
+ */
 @RestController
 @SpringBootApplication
 public class Application {
--- src/main/java/org/isooproject/hello/HelloService.java
+++ src/main/java/org/isooproject/hello/HelloService.java
@@ -13,7 +13,11 @@ import java.util.stream.Collectors;
 @Service
 public class HelloService {
 
+    // properties value를 가져다 쓰는 방
+    @Value("${name}")
+    String name;
+
     public String getMessage() {
-        return "hello world!";
+        return "hello, " + name;
     }
 }
