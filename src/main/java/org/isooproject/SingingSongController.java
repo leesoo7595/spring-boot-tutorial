60247bfbf0240466ef362c01b7c3705c994413b0
leesoo <isoo7510@gmail.com>
2018-10-22 01:08:21 +0900

    27.1.13 CORS Support - CrossOrigin 어노테이션

--- src/main/java/org/isooproject/SingingSongController.java
+++ src/main/java/org/isooproject/SingingSongController.java
@@ -1,11 +1,13 @@
 package org.isooproject;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SingingSongController {
 
+    @CrossOrigin
     @GetMapping("/sing")
     public SingingSong singingSong() {
         SingingSong singingSong = new SingingSong();
