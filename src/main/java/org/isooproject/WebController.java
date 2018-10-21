ac2e029370199f6507f00d3f9345a03ca5f63d32
leesoo <isoo7510@gmail.com>
2018-10-21 16:03:22 +0900

    27.1.10 Template Engines - freemarker: hello leesoo 띄우기

---
+++ src/main/java/org/isooproject/WebController.java
@@ -0,0 +1,21 @@
+package org.isooproject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WebController {
+
+    /**
+     * Viewname (String) --> ViewResolver --> View(view)
+     * @param model
+     * @return
+     */
+    @GetMapping("/hello")
+    public String hello(Model model, @RequestParam String name) {
+        model.addAttribute("name", "leesoo");
+        return "hello";
+    }
+}
