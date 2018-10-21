ec14c4209bad2ceb1152cc93dcc01ffe03854871
leesoo <isoo7510@gmail.com>
2018-10-21 20:18:59 +0900

    27.1.9 ConfigurableWebBindingInitializer - 3) Bean으로 등록하여 자동 형태 변환

--- src/main/java/org/isooproject/LibraryConverter.java
+++ src/main/java/org/isooproject/LibraryConverter.java
@@ -1,9 +1,11 @@
 package org.isooproject;
 
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
 // String을 Library 형태로 컨버터시켜준다.
-public class LibraryConverter implements Converter<String, Library> {
+@Component
+public class  LibraryConverter implements Converter<String, Library> {
 
     @Override
     public Library convert(String id) {
--- src/main/java/org/isooproject/MyWebConfig.java
+++ src/main/java/org/isooproject/MyWebConfig.java
@@ -7,9 +7,4 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MyWebConfig implements WebMvcConfigurer {
 
-    // addConverter에 새로만든 LibraryConverter 추가
-    @Override
-    public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new LibraryConverter());
-    }
 }
