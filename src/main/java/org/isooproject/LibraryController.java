e194b7f31af726518f86ade86142ab3553b0f27f
leesoo <isoo7510@gmail.com>
2018-10-21 21:43:49 +0900

    27.1.12 Spring HATEOAS

--- src/main/java/org/isooproject/LibraryController.java
+++ src/main/java/org/isooproject/LibraryController.java
@@ -1,8 +1,12 @@
 package org.isooproject;
 
+import org.springframework.hateoas.Link;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 @RestController
 public class LibraryController {
@@ -11,10 +15,19 @@ public class LibraryController {
     // 2. 어떻게 넣는가?
     // 3. Converter, Formatter
     @GetMapping("/bs/{id}")
-    public Library visitLibrary(@PathVariable("id") Library library) {
+    public LibraryResource namedLibrary(@PathVariable("id") Library library) {
         if (library.getId() == 100) {
             throw new LibraryException();
         }
-        return library;
+
+        // resource를 새로 만들고!
+        LibraryResource resource = new LibraryResource();
+        resource.setName(library.getId() + "번째 도서관 입니다.");
+        resource.add(new Link("http://localhost:8080/bs/" + library.getId()));
+
+        // link를 만들어서 resource에 추가해주면 !
+        Link link = linkTo(LibraryController.class).slash("bs").slash(library.getId()).withSelfRel();
+        resource.add(link);
+        return resource;
     }
 }
