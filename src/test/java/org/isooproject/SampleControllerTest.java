f72c3ef3be85b1150d6f673281c66bd8f02bff03
leesoo <isoo7510@gmail.com>
2018-10-22 14:25:00 +0900

    44.3.2 Detecting Test Configuration

--- src/test/java/org/isooproject/SampleControllerTest.java
+++ src/test/java/org/isooproject/SampleControllerTest.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,8 +21,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class SampleControllerTest {
 
+    // 설정 파일을 달고 있는 inner class를 만들 경우, static을 붙여줘야한다.
+    @TestConfiguration
+    static class TestConfig {
+
+        @Bean
+        public String myBean() {
+            return "myBean";
+        }
+    }
+
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    String myBean;
 
     @Test
     public void testFoo() throws Exception {
