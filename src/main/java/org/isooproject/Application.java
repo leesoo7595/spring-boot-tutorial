833529fd8984c45ae0a458ee1b81e0bc581139b1
leesoo <isoo7510@gmail.com>
2018-10-22 15:22:35 +0900

    44.3.5 Testing with a running server - WebTestClient(Webflux기반) 사용

--- src/main/java/org/isooproject/Application.java
+++ src/main/java/org/isooproject/Application.java
@@ -2,10 +2,6 @@ package org.isooproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.convert.support.ConfigurableConversionService;
-import org.springframework.format.support.FormattingConversionService;
-import org.springframework.web.bind.support.ConfigurableWebBindingInitializer;
 
 @SpringBootApplication
 public class Application {
--- src/test/java/org/isooproject/SampleControllerTest.java
+++ src/test/java/org/isooproject/SampleControllerTest.java
@@ -1,57 +1,42 @@
 package org.isooproject;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
 public class SampleControllerTest {
 // 랜덤포트로 웹어플리케이션컨텍스트 띄우기 (진짜 웹어플리케이션컨텍스트가 띄워진닷!)
     @LocalServerPort
     int port;
 
+    // webflux 기반으로만 쓸 수 있는 아이..
     @Autowired
-    MockMvc mockMvc;
-
-    @Autowired
-    WebApplicationContext wac;
+    WebTestClient webTestClient;
 
     @Autowired
     TestRestTemplate testRestTemplate;
 
     @Test
-    public void testFoo() throws Exception {
-        System.out.println("==============PORT=============");
-        System.out.println(port);
-
-        assertThat(mockMvc).isNotNull();
-        mockMvc.perform(get("/foo"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("Hello"))
-                .andDo(print());
-    }
-
-    @Test
     public void testFooWithRestTemplate() {
         String body = this.testRestTemplate.getForObject("/foo", String.class);
         assertThat(body).isEqualTo("Hello");
+    }
+
+    @Test
+    public void testFooWithWebTestClient() {
+        webTestClient.get().uri("/foo").exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("Hello");
     }
 
 }
