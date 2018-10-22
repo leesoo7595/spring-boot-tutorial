fa790a2bbe0f1b63335affaef7bcffa2c9714e4d
leesoo <isoo7510@gmail.com>
2018-10-22 14:39:29 +0900

    44.3.3 Excluding Test Configuration

--- src/test/java/org/isooproject/SampleControllerTest.java
+++ src/test/java/org/isooproject/SampleControllerTest.java
@@ -5,10 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,31 +18,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@Import(TestConfig.class)
 public class SampleControllerTest {
-
-    // 설정 파일을 달고 있는 inner class를 만들 경우, static을 붙여줘야한다.
-//    @TestConfiguration
-    // 메인 설정으로 등록되게해서 그 안에 있는 빈을 제외하고 아무것도 없음. -> componentscan이 없다.
-    // 밖에 있는 SampleController를 빈으로 찾지 못한다.
-    // 베이스패키지 기준으로 Application.class를 찾는다 -> SampleController를 찾을 수 있게됨
-    @Configuration
-    @ComponentScan(basePackageClasses = Application.class)
-    static class TestConfig {
-
-        @Bean
-        public String myBean() {
-            return "myBean";
-        }
-    }
 
     @Autowired
     MockMvc mockMvc;
 
     @Autowired
     String myBean;
-
-    @Autowired
-    SampleController sampleController;
 
     @Test
     public void testFoo() throws Exception {
