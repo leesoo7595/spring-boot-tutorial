d33b5148a69bca4c085b778ee37357d7847f1d5d
leesoo <isoo7510@gmail.com>
2018-10-23 10:50:44 +0900

    44.3.11 Auto-configured Spring WebFlux Tests

--- src/test/java/org/isooproject/SampleWebFluxTest.java
+++ src/test/java/org/isooproject/SampleWebFluxTest.java
@@ -3,27 +3,22 @@ package org.isooproject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 // SampleController만 테스트할 때 그것만 빈으로 만들어줌
 // @WebMvcTest(SampleController.class)
 // 디폴트는 모든 컨트롤러를 빈으로 만들어줌
 @RunWith(SpringRunner.class)
-@WebMvcTest
-public class SampleWebMvcTest {
+@WebFluxTest
+public class SampleWebFluxTest {
 
     @Autowired
-    MockMvc mockMvc;
+    WebTestClient webTestClient;
 
     //SpyBean 같은 경우 원래있는 빈을 감싸서 쓰는 것이기 때문에, 빈이 없는 관계로 쓰지 못한다.
     //@Service는 빈으로 등록해주지 않기때문에(@WebMvcTest) 따로 MockBean으로 등록해주어야한다.
@@ -34,11 +29,9 @@ public class SampleWebMvcTest {
     public void testFoo() throws Exception {
         // MockBean인 sampleService 설정
         given(sampleService.getName()).willReturn("isooproject");
-        // MockBean 값 확
-        mockMvc.perform(get("/foo"))
-                .andExpect(handler().handlerType(SampleController.class))
-                .andExpect(handler().methodName("foo"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("isooproject"));
+
+        webTestClient.get().uri("/foo").exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("isooproject");
     }
 }
