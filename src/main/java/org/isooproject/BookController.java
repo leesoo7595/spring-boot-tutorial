da949a524de49203247c1bb88a021c0060adf236
leesoo <isoo7510@gmail.com>
2018-10-23 20:04:38 +0900

    28.2 The “Spring WebFlux Framework” - Flux & Mono

--- src/main/java/org/isooproject/BookController.java
+++ src/main/java/org/isooproject/BookController.java
@@ -2,6 +2,8 @@ package org.isooproject;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class BookController {
@@ -9,5 +11,15 @@ public class BookController {
     @GetMapping("/foo")
     public String getFoo() {
         return "isooproject";
+    }
+
+    @GetMapping("/flux")
+    public Flux<String> flux() {
+        return Flux.just("a", "b", "c");
+    }
+
+    @GetMapping("/mono")
+    public Mono<String> mono() {
+        return Mono.just("mono");
     }
 }
