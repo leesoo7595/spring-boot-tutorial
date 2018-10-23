83669bec3a2055be4f534882a6a279db9b2ce8a2
leesoo <isoo7510@gmail.com>
2018-10-24 01:28:53 +0900

    28.2 The “Spring WebFlux Framework” - Handler Flux Example(List)

--- src/main/java/org/isooproject/BookHandler.java
+++ src/main/java/org/isooproject/BookHandler.java
@@ -4,18 +4,23 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Controller
 public class BookHandler {
 
     public Mono<ServerResponse> allBooks(ServerRequest request) {
-        Book book = new Book();
-        book.setIsbn("1123");
-        book.setTitle("isooproject");
+        Book book1 = new Book();
+        book1.setIsbn("1123");
+        book1.setTitle("isooproject");
 
-        Mono<Book> mono = Mono.just(book);
+        Book book2 = new Book();
+        book2.setIsbn("2234");
+        book2.setTitle("springboot");
 
-        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(mono, Book.class);
+        Flux<Book> books = Flux.just(book1, book2);
+
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(books, Book.class);
     }
 }
