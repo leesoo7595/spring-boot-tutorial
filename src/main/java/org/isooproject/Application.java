7afe2d050b32ca02e3cfa857041968d02a4f7860
leesoo <isoo7510@gmail.com>
2018-10-24 13:58:56 +0900

    28.1.3 Custom JSON Serializers and Deserializers - bookHandler를 Application에

--- src/main/java/org/isooproject/Application.java
+++ src/main/java/org/isooproject/Application.java
@@ -7,16 +7,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @SpringBootApplication
 public class Application {
-
-    @Autowired
-    BookHandler bookHandler;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -24,7 +23,20 @@ public class Application {
 
     @Bean
     public RouterFunction<ServerResponse> rount() {
-        return route(GET("/book").and(accept(MediaType.APPLICATION_JSON)), bookHandler::allBooks);
+        return route(GET("/book").and(accept(MediaType.APPLICATION_JSON)),
+                req -> {
+                    Book book1 = new Book();
+                    book1.setIsbn("1123");
+                    book1.setTitle("isooproject");
+
+                    Book book2 = new Book();
+                    book2.setIsbn("2234");
+                    book2.setTitle("springboot");
+
+                    Flux<Book> books = Flux.just(book1, book2);
+
+                    return ok().contentType(MediaType.APPLICATION_JSON).body(books, Book.class);
+                });
     }
 }
 
