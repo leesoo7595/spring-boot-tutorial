437a1bc19bf81101f66a960ba4198000126b0590
leesoo <isoo7510@gmail.com>
2018-11-06 17:14:28 +0900

    30.1.2 Connection to a Production Database - h2 : production DB

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Customers (CustomerName, Address, City, PostalCode) VALUES ('Cardinal','Skagen 21','Stavanger','4006');
+INSERT INTO Customers (CustomerName, Address, City, PostalCode) VALUES ('Leesoo','Kangnam','Seoul','500');
