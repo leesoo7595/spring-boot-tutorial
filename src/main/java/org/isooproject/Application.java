2e5bb29f256e245048dcbcf5200f850afda2bd18
leesoo <isoo7510@gmail.com>
2018-11-06 16:59:48 +0900

    30.1.2 Connection to a Production Database - h2

--- src/main/java/org/isooproject/Application.java
+++ src/main/java/org/isooproject/Application.java
@@ -1,29 +1,13 @@
 package org.isooproject;
 
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public ApplicationRunner runner(JdbcTemplate jdbcTemplate){
-        return new ApplicationRunner() {
-
-            @Override
-            public void run(ApplicationArguments args) throws Exception {
-                Integer count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM Customers", Integer.class);
-                System.out.println("count : " + count);
-            }
-        };
     }
 
 }
--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO Customers (CustomerName, Address, City, PostalCode) VALUES ('Cardinal','Skagen 21','Stavanger','4006');
-INSERT INTO Customers (CustomerName, Address, City, PostalCode) VALUES ('Leesoo','Kangnam','Seoul','500');
