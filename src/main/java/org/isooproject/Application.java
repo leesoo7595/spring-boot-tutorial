dedf10f4220b6ace06686292a8296a5b327d40db
leesoo <isoo7510@gmail.com>
2018-11-06 16:33:59 +0900

    30.1.1 Embedded Database Support - JPA & hsqldb

--- src/main/java/org/isooproject/Application.java
+++ src/main/java/org/isooproject/Application.java
@@ -1,8 +1,11 @@
 package org.isooproject;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class Application {
@@ -10,6 +13,19 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+    public ApplicationRunner runner(JdbcTemplate jdbcTemplate){
+        return new ApplicationRunner() {
+
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                Integer count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM Customers", Integer.class);
+                System.out.println("count : " + count);
+            }
+        };
+    }
+
 }
 
 
