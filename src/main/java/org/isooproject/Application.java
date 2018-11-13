8b8d4bc8de2c2dea4021b533b81ebf5f346aa03c
leesoo <isoo7510@gmail.com>
2018-11-13 14:53:47 +0900

    31.2.1 Connecting to a MongoDB Database - docker에서 mongodb 띄우고 test db 들어갔는지 확인

--- src/main/java/org/isooproject/Application.java
+++ src/main/java/org/isooproject/Application.java
@@ -1,13 +1,36 @@
 package org.isooproject;
 
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDbFactory;
 
 @SpringBootApplication
 public class Application {
 
+    @Autowired
+    MongoDbFactory mongoDbFactory;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return args -> {
+            MongoDatabase db = mongoDbFactory.getDb();
+            db.createCollection("meetings");
+
+            db.getCollection("meetings").insertOne(
+                    new Document()
+                            .append("title", "new meeting")
+                            .append("location", "Seattle")
+            );
+        };
     }
 
 }
--- src/main/java/org/isooproject/MeetingRepository.java
+++
@@ -1,9 +0,0 @@
-package org.isooproject;
-
-import org.springframework.data.redis.core.RedisHash;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MeetingRepository extends CrudRepository<Meeting, String> {
-
-
-}
